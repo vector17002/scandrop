6fe6c2b9556ff9c6b6726858caf476257480dbb8
vector17002 <vector17002>
2026-08-02 22:43:38 +0530

    added prod updates

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -11,7 +11,8 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'http://scandrop-alb-1254203703.ap-south-1.elb.amazonaws.com',
+        // target: "http://localhost:3000",
         changeOrigin: true,
       },
     },
