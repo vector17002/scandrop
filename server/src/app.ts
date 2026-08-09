ed198dce41441ad41cb45675dc36eefcacea956f
vector17002 <vector17002>
2026-08-09 17:55:36 +0530

    deployed backend to render

--- server/src/app.ts
+++ server/src/app.ts
@@ -7,7 +7,7 @@ import rateLimiterMiddleware from "./middleware/ratelimiterMiddleware.js";
 const app = express();
 
 app.use(cors({
-    origin: process.env.ALLOWED_ORIGINS?.split(',') ??  ['http://localhost:3000', "http://localhost:5173"],
+    origin: process.env.ALLOWED_ORIGINS?.split(',') ??  ["https://scandrop-ihuc.onrender.com,https://scandrop-8v9p0zpve-geek-sanemi.vercel.app"],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
